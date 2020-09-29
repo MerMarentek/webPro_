4523e8daee63dfb08539b69571e9a7e7c0ad22eb
mer83 <mercymarentek1@gmail.com>
2020-09-29 21:15:30 +0800

    pertemuan11

---
+++ pertemuan#11.js
@@ -0,0 +1,54 @@
+
+//pertemuan 11
+
+// function ucapkanSalam() {
+//     return "Selamat Pagi";
+// };
+
+// // Arrow function
+
+
+// const namaFungsi = (parameter1, parameter2) => {
+//     // kode yang ingin dijalankan dalam fungsi
+// };
+
+//  jika fungsi tidak memiliki parameter sama sekali
+
+// const namaFungsiTanpaParameter = () => {
+//     // kode yang ingin dijalankan dalam fungsi
+// }
+
+//fungsi dengan parameter
+
+// const operasiPenjumlahan = (bilangan1, bilangan2) => {
+//     const hasil =  bilangan1 + bilangan2;
+//     return hasil;
+// };
+  
+// console.log(operasiPenjumlahan(3, 4));
+  
+// // fungsi yang tidak memiliki parameter
+
+// const namaJenisAnjing = () => {
+//     const anjing = ["Pug", "Bulldog", "Poodle"];
+//     return anjing[Math.floor(Math.random()*(anjing.length))];
+// }
+  
+// console.log(namaJenisAnjing()); 
+
+
+//Exercise#3
+
+//Convert to ES6 Syntax
+
+
+// const yearUntilRetirement=(years, firstName) => years = retirement - age, firstName ="Mer" 
+//    const age = 25;
+//    const retirement = 60; 
+    
+//     console.log(yearUntilRetirement());
+//     if (retirement>0){
+//         console.log(firstName + " retirement in "+retirement+" years");
+//     } else {
+//         console.log(firstName + "is already retired.");
+//     }
